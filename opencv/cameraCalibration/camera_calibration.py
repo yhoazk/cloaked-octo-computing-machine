8b3d7a375a8315460b87533e039735c770cfe270
porko <yhoazk@gmail.com>
2017-02-01 23:33:58 -0600

    Added folder for opencv projects

---
+++ opencv/cameraCalibration/camera_calibration.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    plt.imshow(cv2.imread("pattern.jpg"))
+    plt.show()
