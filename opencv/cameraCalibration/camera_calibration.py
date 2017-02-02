3bed00a283f09950b53135b0211aa8b908b4daa3
porko <yhoazk@gmail.com>
2017-02-01 23:51:59 -0600

    Working example

--- opencv/cameraCalibration/camera_calibration.py
+++ opencv/cameraCalibration/camera_calibration.py
@@ -6,19 +6,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Image properties
-nx = 7 # number of inside corners in x
-ny = 7 # number of inside corners in y
+nx = 6 # number of inside corners in x
+ny = 9 # number of inside corners in y
 
 
 if __name__ == "__main__":
-    img = cv2.imread("pattern.jpg")
+    img = cv2.imread("pattern2.jpg")
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # find the chessboard corners
     ret, corners = cv2.findChessboardCorners(gray_img, (nx,ny), None)
-#    if ret == True:
+    if ret == True:
 #Draw and displaythe corners
-    cv2.drawChessboardCorners(img, (nx,ny), corners, ret)
-    plt.imshow(img)
-    plt.show()
-#    else:
-#        print("Error", ret, corners)
+        cv2.drawChessboardCorners(img, (nx,ny), corners, ret)
+        plt.imshow(img)
+        plt.show()
+    else:
+        print("Error", ret, corners)
