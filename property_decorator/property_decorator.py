7af2935ddd2c42ca32c961f7dc982317c81e4c73
Gustavo Espindola <yhoazk@gmail.com>
2017-02-15 11:08:05 -0600

    Update property_decorator.py

--- property_decorator/property_decorator.py
+++ property_decorator/property_decorator.py
@@ -1,6 +1,45 @@
 #!/usr/bin/env python
 
-class no_decor():
-    def __init__(self, temp):
+class Celsius:
+    def __init__(self, temperature=0):
+        self.set_temperature(temperature)
 
+    def to_f(self):
+        return (self.get_temperature() * 1.8) + 32
     
+    ## needed implementation
+    def get_temperature(self):
+        print("get the value")
+        return self._temperature
+
+    def set_temperature(self, value):
+        print(" get the value")
+        if value < -273:
+            raise ValueError("Temperature below -273 not possible")
+        self._temperature = value
+
+    temperature = property(get_temperature, set_temperature) 
+
+class Celsius_decorated:
+    def __init__(self, temperature = 0):
+        self._temperature = temperature
+
+    def to_fahrenheit(self):
+        return (self.temperature * 1.8) + 32
+
+    @property
+    def temperature(self):
+        print("Getting value")
+        return self._temperature
+
+    @temperature.setter
+    def temperature(self, value):
+        if value < -273:
+            raise ValueError("Temperature below -273 is not possible")
+        print("Setting value")
+        self._temperature = value
+
+
+t = Celsius(6)
+print(t)
+
