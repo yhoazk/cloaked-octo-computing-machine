7dc58f250415096632a7b724151c2ee380611405
Gustavo Alan Espíndola Perez <yhoazk@gmail.com>
2017-09-26 09:51:26 +0200

    Further examples on decorators

--- decorators/decorators.py
+++ decorators/decorators.py
@@ -1,3 +1,5 @@
+""" Test decorators #1 """
+
 def print_args(function):
     def wrapper(*args, **kwargs):
         print ('Arguments:', args, kwargs)
@@ -10,7 +12,7 @@ def write(text):
 
 write('foo')
 
-
+print("-"*80)
 # Example 2
 
 def star(fnc):
@@ -26,7 +28,8 @@ def percent(fnc):
         fnc(*args,**kwargs)
         print('%'*30)
     return inner
-
+# Cascading decorators, a decorator takes a function as arguemnt and must return
+# another function as result. Subsitutes the called function with the given function
 @star
 @percent
 def printer(msg):
@@ -35,3 +38,42 @@ def printer(msg):
 printer("asdas")
 
 
+
+print(""" Decorator  Example #3""")
+
+
+# The decorator takes a function as input argument and returns
+# a function which calls that function.
+
+def correct_plus5(fnc):
+    def wrap(msg):
+        return fnc("+{}+".format(msg))
+    return wrap
+
+@correct_plus5
+def other_print(msg):
+    print(msg)
+
+# Call the function
+other_print("----")
+
+print("Example #4: a decorator generator")
+
+"""
+Passing arguments to decorators
+This example demostrates how to pass a parameter to a decorator, to make it more
+generic
+"""
+
+def tags(tag_name):
+    def tag_decorator(fnc):
+        def fnc_wrapper(string):
+            return "<{0}>{1}<{0}>".format(tag_name, fnc(string))
+        return fnc_wrapper
+    return tag_decorator
+
+@tags("xx")
+def catme(text):
+    return "test: " + text
+
+print(catme("stuff"))
