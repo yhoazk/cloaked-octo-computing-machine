5f46df93fa8c96f36e85b98a254b54cf333702e3
yhoazk <yhoazk@gmail.com>
2018-10-05 23:20:55 +0200

    Improve parsing

--- unittest/xunitparser/xunit.py
+++ unittest/xunitparser/xunit.py
@@ -4,17 +4,21 @@
 import xunitparser
 
 def get_staticstics(path):
-    ts, tr = xunitparser.parse(path)
-    #print(dir(tr))
+    _, tr = xunitparser.parse(path)
     failed = len(tr.failures)
     test = tr.testsRun
     skipped = len(tr.skipped)
-    details = ["Test Name: {} \n {}".format(tc[0].methodname, tc[0].message) for tc in tr.failures]
-    #print(len(tr.failures))
-    #a,b = tr.failures[0]
-    #print("-"*40 + "\n" + a.message)
-    #print("Method name: " + a.methodname)
-    #print(b)
+    details = ["Test Name: {} \nTest Series: {} \n{}".format(tc.methodname, tc.classname, tc.message) for tc, _ in tr.failures]
     print("\n\n".join(details))
     print("Failed {}  Total Test: {} Skipped {}".format(failed, test, skipped))
+
 get_staticstics("../samples/gtest_with_failure.xml")
+print(":"*80)
+get_staticstics("../samples/sample.xml")
+print(":"*80)
+get_staticstics("../samples/sample2.xml")
+print(":"*80)
+get_staticstics("../samples/sample3.xml")
+print(":"*80)
+get_staticstics("../samples/test.xml")
+print(":"*80)
