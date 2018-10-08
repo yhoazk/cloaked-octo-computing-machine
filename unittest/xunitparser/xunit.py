9752ec1c1b9afe9bafb5650fa46f166d3e1f697b
yhoazk <yhoazk@gmail.com>
2018-10-08 23:15:08 +0200

    Parsing the disabled attribute in xmls

--- unittest/xunitparser/xunit.py
+++ unittest/xunitparser/xunit.py
@@ -2,6 +2,10 @@
 
 
 import xunitparser
+from xml.etree import ElementTree
+'''
+How to get the disabled test?
+'''
 
 def get_staticstics(path):
     _, tr = xunitparser.parse(path)
@@ -10,7 +14,15 @@ def get_staticstics(path):
     skipped = len(tr.skipped)
     details = ["Test Name: {} \nTest Series: {} \n{}".format(tc.methodname, tc.classname, tc.message) for tc, _ in tr.failures]
     print("\n\n".join(details))
-    print("Failed {}  Total Test: {} Skipped {}".format(failed, test, skipped))
+
+    xml  = ElementTree.parse(path).getroot()
+    disabled = 0
+    if xml.tag == 'testsuites' and 'disabled' in xml.attrib:
+        disabled = int(xml.attrib['disabled'])
+    else:
+        # disabled not in testsuites, collect the disabled tests in testsuite
+        disabled = sum(int(ts.attrib['disabled']) for ts in xml if ts.tag == 'testsuite' and 'disabled' in ts.attrib)
+    print("Failed {}  Total Test: {} Skipped {} Disabled {}".format(failed, test, skipped, disabled))
 
 get_staticstics("../samples/gtest_with_failure.xml")
 print(":"*80)
@@ -21,4 +33,6 @@ print(":"*80)
 get_staticstics("../samples/sample3.xml")
 print(":"*80)
 get_staticstics("../samples/test.xml")
+print(":"*80)
+get_staticstics("../samples/report.xml")
 print(":"*80)
